f30f17c886e4f8c8a75ef476c37271c11330dd27
Brian Grad <briangrad@brians-mbp-2.attlocal.net>
2018-02-09 23:19:53 -0600

    images

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,1 +1,33 @@
-javascript
+$(document).ready(function(){
+    var characters= {
+        "Obi-Wan":{
+            name: "Obi-Wan",
+            health: 120,
+            attack: 8,
+            imageUrl: url(https://lumiere-a.akamaihd.net/v1/images/Obi-Wan-Kenobi_6d775533.jpeg?region=0%2C0%2C1536%2C864&width=768),
+            enemyAttackBack: 15
+        },
+        "Luke-Skywalker":{
+            name: "Luke",
+            health: 100,
+            attack: 10,
+            imageUrl: url(https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg/revision/latest?cb=20170927034529),
+            enemyAttackBack: 10
+        },
+
+        "Darth-Maul:{
+            name: "Darth-Mall",
+            health: 110,
+            attack: 12,
+            imageUrl: url(https://vignette.wikia.nocookie.net/starwars/images/5/50/Darth_Maul_profile.png/revision/latest?cb=20140209162228),
+            enemyAttackBack: 12
+        },
+
+        "Darth-Vader"
+            name: "Darth-Vader",
+            health: 130,
+            attack: 12,
+            imageUrl: url(https://www.sideshowtoy.com/wp-content/uploads/2014/07/star-wars-darth-vader-life-size-feature-400184.jpg),
+            enemyAttackBack: 10
+    
+});
